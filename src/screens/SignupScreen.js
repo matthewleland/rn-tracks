15941d1115260085c98890cc11f557eeef8b11ac
Matthew Leland <mleland18@apu.edu>
2022-11-28 07:50:51 -0800

    wip

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,49 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Text, Input, Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <>
-      <Text>SignupScreen</Text>
-      <Button
-        title='go to signin'
-        onPress={() => navigation.navigate('Signin')}
+    <View style={styles.container}>
+      <Spacer>
+        <Text h4>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label='Email'
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize='none'
+        autoCorrect={false}
       />
-    </>
+      <Input
+        secureTextEntry
+        label='Password'
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
+      <Spacer>
+        <Button title='Sign Up' />
+      </Spacer>
+    </View>
   )
 }
 
-const styles = StyleSheet.create({})
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 200,
+  },
+})
 
 export default SignupScreen
