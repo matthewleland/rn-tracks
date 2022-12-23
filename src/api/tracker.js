c5988c44ed8c45084830629ac2f7b66245bb0357
Matthew Leland <mleland18@apu.edu>
2022-12-22 23:37:15 -0800

    wip

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const instance = axios.create({
-  baseURL: 'http://228b-47-146-226-34.ngrok.io',
+  baseURL: '',
 })
 
 instance.interceptors.request.use(
